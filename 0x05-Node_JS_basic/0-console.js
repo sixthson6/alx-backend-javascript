0b20005b5b6f46ead63a3fb70d0c18e8cce51365
sixthson6 <sudais2408@gmail.com>
2024-11-28 07:03:03 +0000

    add description

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,8 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
 function displayMessage(message) {
     console.log(message);
 }
