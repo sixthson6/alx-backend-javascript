eeb57e6827d5bed58b557669aafa1a30e3b3cc1a
sixthson6 <sudais2408@gmail.com>
2024-11-29 06:03:52 +0000

    read input

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,19 @@
+/**
+ * It should display the message Welcome to Holberton School, what is your name? (followed by a new line)
+The user should be able to input their name on a new line
+The program should display Your name is: INPUT
+When the user ends the program, it should display This important software is now closing (followed by a new line)
+ */
+function userGreet() {
+    process.stdout.write("Welcome to Holberton School, what is your name?\n")
+
+    process.stdin.on('data', (data) => {
+        process.stdout.write(`Your name is: ${data}`)
+    })
+
+    process.stdin.on('end', () => {
+        process.stdout.write("This important software is closing\n")
+    })
+}
+
+userGreet()
